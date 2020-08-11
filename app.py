1eb12e57c70feeb61b7fd51ec10e790cbfe36aa1
TechyNilesh <49296182+TechyNilesh@users.noreply.github.com>
2020-08-11 06:56:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, request
 import pickle
 import re
 import ftfy
-
+import nltkmodules
+from textblob import TextBlob
 
 app = Flask(__name__)
 
@@ -11,6 +12,23 @@ app = Flask(__name__)
 filename = 'covid-msg-lr-model.pkl'
 model = pickle.load(open(filename, 'rb'))
 vect = pickle.load(open('tf-vect-transform.pkl', 'rb'))
+
+# Create a function to get the subjectivity
+def getSubjectivity(text):
+   return TextBlob(text).sentiment.subjectivity
+
+# Create a function to get the polarity
+def getPolarity(text):
+   return  TextBlob(text).sentiment.polarity
+
+# Create a function to compute negative (-1), neutral (0) and positive (+1) analysis
+def getAnalysis(score):
+    if score < 0:
+        return 'Negative'
+    elif score == 0:
+        return 'Neutral'
+    else:
+        return 'Positive'
 
 #text cleaning
 def cleanTxt(text):
@@ -45,8 +63,11 @@ def predict():
         prediction = predcovidmsg(text)
         bar_labels=['Not COVID-19 Related','COVID-19 Related']
         bar_values=[prediction[0][0]*100,prediction[0][1]*100]
+        sub = getSubjectivity(text)
+        pol = getPolarity(text)
+        sentiment = getAnalysis(pol)
         print(bar_values)
-        return render_template('index.html',labels=bar_labels, values=bar_values,text=text)
+        return render_template('index.html',labels=bar_labels, values=bar_values,text=text,sub=sub,pol=pol,sentiment=sentiment)
 
 if __name__ == '__main__':
     app.run(debug=True)
